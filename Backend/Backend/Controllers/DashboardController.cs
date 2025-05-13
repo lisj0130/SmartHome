7058fb9f4435461c35e9dcf7dd196e4a482c7897
lisj0130 <lisj0130@student.umu.se>
2025-05-13 11:29:21 +0200

    La till kommentarer vi ska följa

--- Backend/Backend/Controllers/DashboardController.cs
+++ Backend/Backend/Controllers/DashboardController.cs
@@ -8,19 +8,25 @@ namespace Backend.Controllers
     public class DashboardController : Controller
     {
 
+        //Visa vyn
         public IActionResult Dashboard()
         {
             return View();
         }
 
+        //Skicka anrop till react-projektet med indikation om vilken lampa som ska tändas. 1 för tända + id
         public IActionResult LightsOn(int id) { }
 
+        //Skicka anrop till react-projektet med indikation om vilken lampa som ska släckas. 0 för släcka + id
         public IActionResult LightsOff(int id) { }
 
+        //Gör en algoritm för att generera inomhustemperatur och returnera den till vyn
         public IActionResult InsideTemp() { }
 
+        //Hämta temperaturen via web api och returnera den till vyn
         public IActionResult OutsideTemp() { }
 
+        //Skapa en algoritm som beräknar elförbrukningen. Ta antal lampor, inomhustemp och utomhustemp i beaktning.
         public IActionResult ElectricityConsumption() { }
     }
 }
