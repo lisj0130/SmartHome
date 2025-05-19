664b4d3df9078053d30c4bc1ab175a70bb8466d8
Valter <vath0008@student.umu.se>
2025-05-19 12:41:22 +0200

    Log och User model.

---
+++ Backend/Backend/Models/Log.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Models
+{
+    public class Log
+    {
+        public int LogID { get; set; } // PK
+        public double InsideTemp { get; set; }
+        public double OutsideTemp { get; set; }
+        public List<string> LightsOn { get; set; } = new List<string>();
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
+    }
+}
---
+++ Backend/Backend/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }  // PK
+        public string GoogleId { get; set; }  
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
