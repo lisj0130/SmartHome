38741b2369dd0531ce38a1597c8975cf47c71dbb
lisj0130 <lisj0130@student.umu.se>
2025-05-19 12:01:19 +0200

    del 4

--- frontend/frontend/src/components/Standlight.jsx
+++ frontend/frontend/src/components/Standlight.jsx
@@ -4,5 +4,5 @@ import { useGLTF } from '@react-three/drei';
 export default function Standlight() {
     const { scene } = useGLTF('/models/standlight.glb');
 
-    return <primitive object={scene} scale={5} position={[-8.8, -3, -8.5]}/>;
+    return <primitive object={scene} scale={6} position={[-8.8, -3, -8.5]}/>;
 }
