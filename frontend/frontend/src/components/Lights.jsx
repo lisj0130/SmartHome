4a21b6e182f5634d5fe5471ec2e410c3784106c1
lisj0130 <lisj0130@student.umu.se>
2025-05-19 12:00:31 +0200

    del 2

--- frontend/frontend/src/components/Lights.jsx
+++ frontend/frontend/src/components/Lights.jsx
@@ -1,8 +1,33 @@
-export default function Lights() {
+export default function Lights({ lampStates }) {
     return (
         <>
             {/* Ljuskällor */}
-            <ambientLight intensity={0.8} /> {/* Mjuk ljuskälla */}
+            <ambientLight intensity={1} /> {/* Mjuk ljuskälla */}
+
+            <pointLight // Taklampa, id = 1
+                color="#fff8dc"             // varmvit färg
+                intensity={lampStates[1] ? 30 : 0}               // starkt ljus
+                position={[0, 2, 0]}        // takposition i mitten av rummet
+                distance={60}               // hur långt ljuset når
+                decay={1.5}                   // realistiskt ljusavtagande
+                castShadow
+            />
+
+            <pointLight // golvlampa, id = 2
+                color="white"       // mjukt varmt ljus, du kan också använda hex t.ex. "#ffdca3"
+                intensity={lampStates[2] ? 10 : 0}           // styrka på ljuset
+                position={[-8.8, 3.2, -8.5]} // placera lite ovanför lampans bas
+                distance={15}           // hur långt ljuset når
+                decay={1.5}               // ljusets avtagande (2 = realistiskt)
+            />
+
+            <pointLight  // dekorationslampa, id = 3
+                color="white"       // mjukt varmt ljus, du kan också använda hex t.ex. "#ffdca3"
+                intensity={lampStates[3] ? 5 : 0}           // styrka på ljuset
+                position={[9, 2, 5]} // placera lite ovanför lampans bas
+                distance={15}           // hur långt ljuset når
+                decay={1.5}               // ljusets avtagande (2 = realistiskt)
+            />
         </>
     );
 }
