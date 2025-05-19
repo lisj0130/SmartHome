fff8ce4b69606eb76156ad5af8ae7bb8a3ca1c22
lisj0130 <lisj0130@student.umu.se>
2025-05-19 12:00:47 +0200

    del 3

--- frontend/frontend/src/components/Plants.jsx
+++ frontend/frontend/src/components/Plants.jsx
@@ -4,9 +4,5 @@ import { useEffect } from 'react';
 export default function Plants() {
     const { scene } = useGLTF('/models/plants.gltf'); // VIKTIGT: absolut sökväg från public/
 
-    useEffect(() => {
-        console.log(scene); // Inspektera detta i DevTools för att hitta luckans namn
-    }, [scene]);
-
-    return <primitive object={scene} scale={5} position={[-8, -2, 4]} />;
+    return <primitive object={scene} scale={5} position={[-8.4, -3, 5]} rotation={[0, 0.4 * Math.PI, 0]} />;
 }
