bb38bbdd9b75b5a217770547697901834b7aaeaf
Valter Thomas Fyrberg <vath0008@ad.umu.se>
2025-05-21 08:52:43 +0200

    Lagt till log i APIController.

--- Backend/Backend/Controllers/APIController.cs
+++ Backend/Backend/Controllers/APIController.cs
@@ -1,3 +1,4 @@
+using Backend.Models;
 using ChatAppBackend.Hubs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
@@ -10,16 +11,34 @@ namespace Backend.Controllers
     public class APIController : ControllerBase
     {
         private readonly IHubContext<SmartHomeHub> _hubContext;
+        private readonly SmartHomeContext _context;
 
-        public APIController(IHubContext<SmartHomeHub> hubContext)
+        public APIController(IHubContext<SmartHomeHub> hubContext, SmartHomeContext context)
         {
             _hubContext = hubContext;
+            _context = context;
         }
 
         //Skicka till react att lampan ska tändas
         [HttpPost("LightsOn/{id}")]
         public async Task<IActionResult> LightsOn(int id)
         {
+            string lightId = $"Light{id}";
+
+            // Hämta senaste loggen eller skapa ny
+            var latestLog = _context.Logs.OrderByDescending(l => l.TimeStamp).FirstOrDefault();
+
+            if (latestLog == null || (DateTime.UtcNow - latestLog.TimeStamp).TotalMinutes > 1)
+            {
+                latestLog = new Log();
+                _context.Logs.Add(latestLog);
+            }
+
+            if (!latestLog.LightsOn.Contains(lightId))
+                latestLog.LightsOn.Add(lightId);
+
+            latestLog.TimeStamp = DateTime.UtcNow;
+            _context.SaveChanges();
             await _hubContext.Clients.All.SendAsync("TurnOnLight", id, 1);
 
             return Ok(new { status = "Lampan är tänd", id = id });
@@ -29,6 +48,26 @@ namespace Backend.Controllers
         [HttpPost("LightsOff/{id}")]
         public async Task<IActionResult> LightsOff(int id)
         {
+            string lightId = $"Light{id}";
+
+            // Hämta senaste logg eller skapa en ny om det gått för lång tid
+            var latestLog = _context.Logs
+                .OrderByDescending(l => l.TimeStamp)
+                .FirstOrDefault();
+
+            if (latestLog == null || (DateTime.UtcNow - latestLog.TimeStamp).TotalMinutes > 1)
+            {
+                latestLog = new Log();
+                _context.Logs.Add(latestLog);
+            }
+
+            // Ta bort lampan om den är tänd
+            if (latestLog.LightsOn.Contains(lightId))
+                latestLog.LightsOn.Remove(lightId);
+
+            latestLog.TimeStamp = DateTime.UtcNow;
+            _context.SaveChanges();
+
             await _hubContext.Clients.All.SendAsync("TurnOffLight", id, 0);
 
             return Ok(new { status = "Lampan är släckt", id = id });
