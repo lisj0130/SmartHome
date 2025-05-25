55134a2540bd257fac90f1fb7f58d75f3263d6ba
Valter <vath0008@student.umu.se>
2025-05-25 19:23:12 +0200

    Small fix.

--- Backend/Backend/Controllers/DashboardController.cs
+++ Backend/Backend/Controllers/DashboardController.cs
@@ -48,9 +48,6 @@ namespace Backend.Controllers
 
             var logs = _context.Logs.OrderByDescending(l => l.TimeStamp).ToList();
 
-            //var consumption = ElectricityConsumption();
-            //ViewData["Consumption"] = consumption;
-
             return View(logs); // Skickar listan av loggar till vyn
         }
 
@@ -61,35 +58,6 @@ namespace Backend.Controllers
 
             return insideTemperature;
         }
-
-        ////Skapa en algoritm som beräknar elförbrukningen
-        //private Dictionary<string, double> ElectricityConsumption()
-        //{
-        //    var logs = _context.Logs
-        //        .OrderBy(l => l.TimeStamp)
-        //        .ToList();
-
-        //    var totalConsumption = new Dictionary<string, double>();
-
-        //    for (int i = 0; i < logs.Count - 1; i++)
-        //    {
-        //        var currentLog = logs[i];
-        //        var nextLog = logs[i + 1];
-
-        //        var duration = (nextLog.TimeStamp - currentLog.TimeStamp).TotalHours;
-
-        //        var lampsStillOn = currentLog.LightsOn.Intersect(nextLog.LightsOn);
-        //        foreach (var lamp in lampsStillOn)
-        //        {
-        //            if (!totalConsumption.ContainsKey(lamp))
-        //                totalConsumption[lamp] = 0;
-
-        //            totalConsumption[lamp] += duration * 0.04; // 40 watt = 0.04 kWh
-        //        }
-        //    }
-
-        //    return totalConsumption;
-        //}
 
         //Hämta in en lista och visa tidigare loggar (5st)
         public IActionResult ShowPreviousLogs()
